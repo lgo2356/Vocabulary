8b48b39eddec9dfa5058a053463ba2005e1bd8e6
lgo2356 <wmf85276@gmail.com>
2021-05-27 18:04:14 +0900

    Custom ProgressBar test

--- app/src/main/java/com/hun/vocabulary/MainActivity.kt
+++ app/src/main/java/com/hun/vocabulary/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.hun.vocabulary
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.hun.vocabulary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.progressLearningRate.apply {
+            setProgress(50)
+            setProgressColor(Color.BLUE)
+        }
     }
 }
---
+++ app/src/main/java/com/hun/vocabulary/view/LearningProgressBar.kt
@@ -0,0 +1,99 @@
+package com.hun.vocabulary.view
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+
+class LearningProgressBar(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    companion object {
+        private const val MAX_PROGRESS = 100
+        private const val MAX_SWEEP_ANGLE = 360
+        private const val ANIMATION_DURATION = 400L
+    }
+
+    private var mRect: Rect = Rect()
+    private var mPaint: Paint = Paint()
+    private var mWidth: Int = 0
+    private var mHeight: Int = 0
+    private var mStartAngle = -90f
+    private var mSweepAngle = 0f
+    private var mProgressColor = Color.BLACK
+    private var mStrokeWidth = 20
+    private var mTextColor = Color.BLACK
+
+    override fun onDraw(canvas: Canvas) {
+        drawOutlineArc(canvas)
+        drawText(canvas)
+
+        super.onDraw(canvas)
+    }
+
+    private fun drawOutlineArc(canvas: Canvas) {
+        val strokeWidth = 20F
+        val diameter: Float = Math.min(width, height) - strokeWidth * 2
+        val outerOval = RectF(strokeWidth, strokeWidth, diameter, diameter)
+
+        mPaint.apply {
+            color = Color.BLACK
+            setStrokeWidth(strokeWidth)
+            isAntiAlias = true
+            strokeCap = Paint.Cap.ROUND
+            style = Paint.Style.STROKE
+        }
+
+        canvas.drawArc(outerOval, mStartAngle, mSweepAngle, false, mPaint)
+    }
+
+    private fun drawText(canvas: Canvas) {
+        mPaint.apply {
+            textSize = Math.min(width, height) / 5f
+            textAlign = Paint.Align.CENTER
+            strokeWidth = 0f
+            color = Color.BLACK
+        }
+
+        val xPos: Float = canvas.width / 2f
+        val yPos: Float = (canvas.height / 2) - (mPaint.descent() + mPaint.ascent() / 2)
+
+        canvas.drawText("${calcProgressFromSweepAngle(mSweepAngle)}%", xPos, yPos, mPaint)
+    }
+
+    private fun calcSweepAngleFromProgress(progress: Int): Float {
+        return ((MAX_SWEEP_ANGLE / MAX_PROGRESS) * progress).toFloat()
+    }
+
+    private fun calcProgressFromSweepAngle(sweepAngle: Float): Int {
+        return ((sweepAngle * MAX_PROGRESS) / MAX_SWEEP_ANGLE).toInt()
+    }
+
+    fun setProgress(progress: Int) {
+        val animator = ValueAnimator.ofFloat(mSweepAngle, calcSweepAngleFromProgress(progress)).apply {
+            interpolator = DecelerateInterpolator()
+            duration = ANIMATION_DURATION
+            addUpdateListener {
+                mSweepAngle = it.animatedValue as Float
+                invalidate()
+            }
+        }
+        animator.start()
+    }
+
+    fun setProgressColor(color: Int) {
+        mProgressColor = color
+        invalidate()
+    }
+
+    fun setProgressWidth(width: Int) {
+        mStrokeWidth = width
+        invalidate()
+    }
+
+    fun setTextColor(color: Int) {
+        mTextColor = color
+        invalidate()
+    }
+}
